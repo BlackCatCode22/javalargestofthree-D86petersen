8afca6e986dd5f8ca1e38da4bc545773f96517d3
Dustin Petersen <d86petersen@gmail.com>
2023-02-19 18:43:25 -0800

    largest of three my code

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -1,74 +1,64 @@
 /* LargestOfThree.java
-*
-* dH 2/15/23
-* Approved solution for Java programming class
-* References:
-*   https://www.w3schools.com/java/java_user_input.asp
-*   https://www.w3schools.com/java/java_conditions.asp
-*   https://www.w3schools.com/java/java_variables.asp
-*
-* input: three ints from user
-* processing: find the largest of the three ints with decision structures
-* output: the largest of three ints, the sum of three ints
+ *
+ * Dustin Petersen
+ * 2/19/2023
+ *
+ * input: three ints from user
+ * processing: find the largest of the three ints with decision structures
+ * output: the largest of three ints, the sum of three ints
  */
 
 import java.util.Scanner;
 
-public class LargestOfThree {
-    public static void main(String[] args) {
-        System.out.println("\n\n The Largest of Three Program \n\n");
+public class LargestOfThree
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\n\n ========== The Largest of Three Program =========== \n\n");
 
-        // Get three ints from the user.
-        int num1 = 0;
-        int num2 = 0;
-        int num3 = 0;
-        int total = 0;
-        int largest = 0;
 
-        // Create a scanner object for user input.
+        // Create a scanner object for user input
         Scanner scanner = new Scanner(System.in);
 
-        // Prompt the user for the first integer.
-        System.out.println("\n\n Please enter an integer for num1: ");
+        // Prompt the user for the first number
+        System.out.println("\n\n Please enter the first number: ");
 
-        // Input user reply to num1
-        num1 = Integer.parseInt(scanner.nextLine());
+        // Input user reply to number 1
+        int n1 = scanner.nextInt();
 
-        // Prompt the user for the second integer.
-        System.out.println("\n\n Please enter the second integer: ");
+        // Prompt the user for the second number
+        System.out.println("\n\n Please enter the second number: ");
 
-        // Input user reply to num1
-        num2 = Integer.parseInt(scanner.nextLine());
+        // Input user reply to number 2
+        int n2 = scanner.nextInt();
 
-        // Prompt the user for the third integer.
+        // Prompt the user for the third number
         System.out.println("\n\n Please enter the third and last integer: ");
 
-        // Input user reply to num1
-        num3 = Integer.parseInt(scanner.nextLine());
+        // Input user reply to number 3
+        int n3 = scanner.nextInt();
 
-        total = num1 + num2 + num3;
 
-        System.out.println("\n The sum of " + num1 + " and " + num2 + " and " + num3 + " is: " + total);
+        // Math the maths to get total of 3 numbers
+        int total = n1 + n2 + n3;
 
-        // Find the largest of three ints with decisions.
-        if (num1 > num2) {
-            if (num1 > num3) {
-                largest = num1;
-            }
-            else {
-                largest = num2;
-            }
+        System.out.println("\n The sum of " + n1 + " and " + n2 + " and " + n3 + " is: " + total);
+
+        // Find the largest of three numbers with nested if statement logic.
+        if (n1 >= n2)
+        {
+            if (n1 >= n3)
+                System.out.println(n1 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
+        } else
+        {
+            if (n2 >= n3)
+                System.out.println(n2 + " is the largest number.");
+            else
+                System.out.println(n3 + " is the largest number.");
         }
-        else {
-            if (num2 > num3) {
-                largest = num2;
-            }
-            else {
-                largest = num3;
-            }
-        }
 
-        System.out.println("\n\n The largest of " + num1 + " and " + num2 + " and " + num3 + " is: " + largest);
 
     }
 }
